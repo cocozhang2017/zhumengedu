7cd8b6e266b0e5397b9e881295d55b2f38dae075
cocozhang <1275899401@qq.com>
2019-09-26 23:52:24 +0800

    555

--- src/pages_mb/home_mb.vue
+++ src/pages_mb/home_mb.vue
@@ -1,27 +1,53 @@
 <template>
     <div id="home">
         <div class="topBox">
-            <img src="../assets/img/top.png">
+            <img src="../assets/mobile/home/home1.jpg"
+                 srcset="../assets/mobile/home/home1-assets/home1@1x.png 320w,
+                        ../assets/mobile/home/home1-assets/home1@2x.png 375w,
+                        ../assets/mobile/home/home1-assets/home1@3x.png 414w"
+            >
             <div class="top_middle">
                 <p>
-                    <img src="../assets/img/guanyu.png" alt="">
+                    <img src="../assets/mobile/home/home7.jpg"
+                         srcset="../assets/mobile/home/home7-assets/home7@1x.png 320w,
+                        ../assets/mobile/home/home7-assets/home7@2x.png 375w,
+                        ../assets/mobile/home/home7-assets/home7@3x.png 414w"
+                    >
                 </p>
 
                 <br/>
-                <img class="about1" src="../assets/img/about.png" alt="">
+                <img  src="../assets/mobile/home/home8.jpg"
+                     srcset="../assets/mobile/home/home8-assets/home8@1x.png 320w,
+                        ../assets/mobile/home/home8-assets/home8@2x.png 375w,
+                        ../assets/mobile/home/home8-assets/home8@3x.png 414w"
+                >
+             `
             </div>
         </div>
         <!--筑梦教育-->
         <div class="zm_edu">
-            <div class="zm_edu_left">
-                <img src="../assets/img/zm_edu.png" alt="">
-            </div>
+            <!--<div class="zm_edu_left">-->
+                <!--<img src="../assets/mobile/home/home2.jpg"-->
+                     <!--srcset="../assets/mobile/home/home2-assets/home@1x.png 320w,-->
+                        <!--../assets/mobile/home/home2-assets/home@2x.png 375w,-->
+                        <!--../assets/mobile/home/home2-assets/home@3x.png 414w"-->
+                <!--&gt;-->
+            <!--</div>-->
             <div class="zm_edu_right">
-                <h2>筑梦教育</h2>
-                <span>
-          诚聘中戏、北电等多所名校命题考官教训内容更是依据备考院校真实题库而来，针对性强、升学率高，
-          总公司旗下的“创造艺人”优先从筑梦教育挖新，使优秀学员完成高考后即可获得影视实践机会
-        </span>
+                <img src="../assets/mobile/home/home3.jpg"
+                     srcset="../assets/mobile/home/home3-assets/home3@1x.png 320w,
+                        ../assets/mobile/home/home3-assets/home3@2x.png 375w,
+                        ../assets/mobile/home/home3-assets/home3@3x.png 414w"
+                >
+                <!--<h6>筑梦教育</h6>-->
+                <!--<span>-->
+                    <!--诚聘中戏、北电等多所名校命题考官-->
+                    <!--&lt;!&ndash;教训内容更是依据备考院校真实题库&ndash;&gt;-->
+                    <!--&lt;!&ndash;而来，针对性强、升学率高，总公司&ndash;&gt;-->
+                    <!--&lt;!&ndash;旗下的"创造艺人"优先从筑梦教育挖&ndash;&gt;-->
+                    <!--&lt;!&ndash;新，使优秀学员完成高考后即可获得&ndash;&gt;-->
+                    <!--&lt;!&ndash;影视实践机会&ndash;&gt;-->
+               <!--</span>-->
             </div>
         </div>
         <!--视频展示-->
@@ -57,7 +83,7 @@
             <div class="CI_content">
                 <div class="CI_content_left">
                     <h3>周末班</h3>
-                    <p>向地方展示我们的师资以及教学力量</p>
+                    <p>向地方展示我们的师资</p>
                     <p>全年周末班</p>
                     <p>地方周末班</p>
                 </div>
@@ -103,8 +129,16 @@
             </p>
             <div class="CE_content">
                 <div class="CE_content_left">
-                    <img class="left" src="../assets/img/school_02.jpg" alt="">
-                    <img class="left" src="../assets/img/school_03.jpg" alt="">
+                    <img src="../assets/mobile/home/home4.jpg"
+                         srcset="../assets/mobile/home/home4-assets/home4@1x.png 320w,
+                        ../assets/mobile/home/home4-assets/home4@2x.png 375w,
+                        ../assets/mobile/home/home4-assets/home4@3x.png 414w"
+                    >
+                    <img src="../assets/mobile/home/home5.jpg"
+                         srcset="../assets/mobile/home/home5-assets/home5@1x.png 320w,
+                        ../assets/mobile/home/home5-assets/home5@2x.png 375w,
+                        ../assets/mobile/home/home5-assets/home5@3x.png 414w"
+                    >
                 </div>
             </div>
         </div>
@@ -131,122 +165,153 @@
                 <span>o</span>
                 <span>l</span>
             </p>
-            <div class="CI_content">
-                <div class="CI_content_left">
-                    <h3>一类院校</h3>
-                    <p>中央戏剧学院</p>
-                    <p>北京电影学院</p>
-                    <p>中国传媒大学</p>
-                    <p>中国戏曲学院</p>
-                    <p>上海戏剧学院</p>
-                    <p>北京舞蹈学院</p>
-                    <p>北京电影学院大专</p>
-                </div>
-                <div class="CI_content_middle">
-                    <h3>二类院校</h3>
-                    <p>北京联合大学</p>
-                    <p>北京城市学院</p>
-                    <p>首都师范大学科德学院</p>
-                    <p>南京广播学院</p>
-                    <p>南京艺术学院</p>
-                    <p>四川音乐学院</p>
-                    <p>浙江传媒学院</p>
-                    <p>北京体育大学</p>
-                    <p>四川传媒大学</p>
-                    <p>湖南大学</p>
-                    <p>同济大学</p>
-                </div>
-                <div class="CI_content_right preparation_school_3 ">
-                    <h3>三类院校</h3>
-                    <p>
-                        <span>天津音乐学院</span>
-                        <span>内蒙古大学</span>
-                    </p>
-                    <p>
-                        <span>辽东师范大学</span>
-                        <span>上海戏剧学院</span>
-                    </p>
-                    <p>
-                        <span>天津师范大学</span>
-                        <span>山东艺术学院</span>
-                    </p>
-                    <p>
-                        <span>河北传媒学院</span>
-                        <span>重庆大学艺术学院</span>
-                    </p>
-                    <p>
-                        <span>谢晋电影学院</span>
-                        <span>上海戏剧学院继续教育学院</span>
-                    </p>
-                    <p>
-                        <span>吉林艺术学院</span>
-                        <span>北京电影学院继续教育学院</span>
-                    </p>
-                    <p>
-                        <span>东北师范大学</span>
-                        <span>北京电影学院(两年北电成</span>
+            <img src="../assets/mobile/home/home9.jpg"
+                 srcset="../assets/mobile/home/home9-assets/home9@1x.png 320w,
+                        ../assets/mobile/home/home9-assets/home9@2x.png 375w,
+                        ../assets/mobile/home/home9-assets/home9@3x.png 414w"
+            >
+            <img src="../assets/mobile/home/home10.jpg"
+                 srcset="../assets/mobile/home/home10-assets/home10@1x.png 320w,
+                        ../assets/mobile/home/home10-assets/home10@2x.png 375w,
+                        ../assets/mobile/home/home10-assets/home10@3x.png 414w"
+            >
+            <!--<h2>备考学校</h2>-->
+            <!--<p>-->
+                <!--<span>P</span>-->
+                <!--<span>r</span>-->
+                <!--<span>e</span>-->
+                <!--<span>p</span>-->
+                <!--<span>a</span>-->
+                <!--<span>r</span>-->
+                <!--<span>a</span>-->
+                <!--<span>t</span>-->
+                <!--<span>i</span>-->
+                <!--<span>o</span>-->
+                <!--<span>n</span>-->
+                <!--&nbsp;&nbsp;-->
+                <!--<span>s</span>-->
+                <!--<span>c</span>-->
+                <!--<span>h</span>-->
+                <!--<span>o</span>-->
+                <!--<span>o</span>-->
+                <!--<span>l</span>-->
+            <!--</p>-->
+            <!--<div class="CI_content">-->
+                <!--<div class="CI_content_left">-->
+                    <!--<h3>一类院校</h3>-->
+                    <!--<p>中央戏剧学院</p>-->
+                    <!--<p>北京电影学院</p>-->
+                    <!--<p>中国传媒大学</p>-->
+                    <!--<p>中国戏曲学院</p>-->
+                    <!--<p>上海戏剧学院</p>-->
+                    <!--<p>北京舞蹈学院</p>-->
+                    <!--<p>北京电影学院大专</p>-->
+                <!--</div>-->
+                <!--<div class="CI_content_middle">-->
+                    <!--<h3>二类院校</h3>-->
+                    <!--<p>北京联合大学</p>-->
+                    <!--<p>北京城市学院</p>-->
+                    <!--<p>首都师范大学科德学院</p>-->
+                    <!--<p>南京广播学院</p>-->
+                    <!--<p>南京艺术学院</p>-->
+                    <!--<p>四川音乐学院</p>-->
+                    <!--<p>浙江传媒学院</p>-->
+                    <!--<p>北京体育大学</p>-->
+                    <!--<p>四川传媒大学</p>-->
+                    <!--<p>湖南大学</p>-->
+                    <!--<p>同济大学</p>-->
+                <!--</div>-->
+                <!--<div class="CI_content_right preparation_school_3 ">-->
+                    <!--<h3>三类院校</h3>-->
+                    <!--<p>-->
+                        <!--<span>天津音乐学院</span>-->
+                        <!--<span>内蒙古大学</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>辽东师范大学</span>-->
+                        <!--<span>上海戏剧学院</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>天津师范大学</span>-->
+                        <!--<span>山东艺术学院</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>河北传媒学院</span>-->
+                        <!--<span>重庆大学艺术学院</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>谢晋电影学院</span>-->
+                        <!--<span>上海戏剧学院继续教育学院</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>吉林艺术学院</span>-->
+                        <!--<span>北京电影学院继续教育学院</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>东北师范大学</span>-->
+                        <!--<span>北京电影学院(两年北电成</span>-->
 
-                    </p>
-                    <p>
-                        <span>西南大学</span>
-                        <span>教+两年纽约电影学院)</span>
-                    </p>
-                    <p>
-                        <span>上海东华大学</span>
-                        <span>安徽大学</span>
-                    </p>
-                    <p>
-                        <span>四川大学</span>
-                        <span>北京工业大学</span>
-                    </p>
-                    <p>
-                        <span>广西艺术学院</span>
-                        <span>南昌大学</span>
-                    </p>
-                    <p>
-                        <span>辽宁大学</span>
-                        <span>陕西师范大学</span>
-                    </p>
-                    <p>
-                        <span>云南艺术学院</span>
-                        <span>天津大学</span>
-                    </p>
-                    <p>
-                        <span>河海大学</span>
-                        <span></span>
-                    </p>
-                    <p>
-                        <span>安庆师范大学</span>
-                        <span></span>
-                    </p>
-                </div>
-            </div>
-            <div class="ps_footer">
-                <div class="ps_footer-left">
-                    <h3>战略合作三类学校</h3>
-                    <p>现代音乐学院(表演、舞蹈表演、音乐表演)</p>
-                    <p>上海音乐学院(表演系)</p>
-                    <p>北京开放大学影视艺术学院</p>
-                    <p>北京电影学院现代创意媒体学院(表演系)拿成人教育学历</p>
-                </div>
-                <div class="ps_footer-right">
-                    <h3>战略合作海外学校</h3>
-                    <p>韩国民族大学</p>
-                    <p>首尔艺术大学</p>
-                    <p>
-                        著名演员车太贤、安
-                    </p>
-                    <p>
-                        在旭、孙艺珍、张赫、
-                    </p>
-                    <p>
-                        崔民秀、金荷娜、申东
-                    </p>
-                    <p>
-                        烨、刘在石
-                    </p>
-                </div>
-            </div>
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>西南大学</span>-->
+                        <!--<span>教+两年纽约电影学院)</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>上海东华大学</span>-->
+                        <!--<span>安徽大学</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>四川大学</span>-->
+                        <!--<span>北京工业大学</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>广西艺术学院</span>-->
+                        <!--<span>南昌大学</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>辽宁大学</span>-->
+                        <!--<span>陕西师范大学</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>云南艺术学院</span>-->
+                        <!--<span>天津大学</span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>河海大学</span>-->
+                        <!--<span></span>-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--<span>安庆师范大学</span>-->
+                        <!--<span></span>-->
+                    <!--</p>-->
+                <!--</div>-->
+            <!--</div>-->
+            <!--<div class="ps_footer">-->
+                <!--<div class="ps_footer-left">-->
+                    <!--<h3>战略合作三类学校</h3>-->
+                    <!--<p>现代音乐学院(表演、舞蹈表演、音乐表演)</p>-->
+                    <!--<p>上海音乐学院(表演系)</p>-->
+                    <!--<p>北京开放大学影视艺术学院</p>-->
+                    <!--<p>北京电影学院现代创意媒体学院(表演系)拿成人教育学历</p>-->
+                <!--</div>-->
+                <!--<div class="ps_footer-right">-->
+                    <!--<h3>战略合作海外学校</h3>-->
+                    <!--<p>韩国民族大学</p>-->
+                    <!--<p>首尔艺术大学</p>-->
+                    <!--<p>-->
+                        <!--著名演员车太贤、安-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--在旭、孙艺珍、张赫、-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--崔民秀、金荷娜、申东-->
+                    <!--</p>-->
+                    <!--<p>-->
+                        <!--烨、刘在石-->
+                    <!--</p>-->
+                <!--</div>-->
+            <!--</div>-->
         </div>
     </div>
 </template>
@@ -284,52 +349,51 @@
 
     .top_middle {
         position: absolute;
-        bottom: 13%;
+        bottom: 12%;
         left: 50%;
         transform: translateX(-50%);
     }
 
     /*筑梦教育*/
     .zm_edu {
-        width: 60%;
+        width: 80%;
         margin: 0 auto;
         position: relative;
-        top: -70px;
-        height: 400px;
+        top: -30px;
         overflow: hidden;
     }
 
-    .zm_edu_left {
-        float: left;
-        width: 66%;
+    /*.zm_edu_left {*/
+        /*float: left;*/
+        /*width: 56%;*/
+        /*height: 100%;*/
 
-    }
+    /*}*/
 
-    .zm_edu_left img {
-        width: 100%;
-    }
+    /*.zm_edu_left img {*/
+        /*width: 100%;*/
+    /*}*/
 
     .zm_edu_right {
-        width: 33%;
+        width: 100%;
         box-sizing: border-box;
         float: left;
         background: #3A3E41;
-        height: 100%;
+        height: 33%;
         color: #fff;
         padding: 0 5%;
     }
 
-    .zm_edu_right > h2 {
-        border: 1px solid #fff;
-        margin: 0 55px;
-        height: 45px;
-        line-height: 45px;
-        margin-top: 60px;
+    .zm_edu_right > h6 {
+        /*border: 1px solid #fff;*/
+        /*margin: 0 20px;*/
+        height: 16px;
+        line-height: 16px;
+        margin-top: 5px;
         text-align: center;
     }
 
     .zm_edu_right > span {
-        margin-top: 20px;
         display: inline-block;
         text-align: left;
     }
@@ -342,10 +406,10 @@
     }
     /*视频介绍*/
     .vd {
-        margin-left:20%;
+        margin-left:10%;
         margin-top:-30px;
-        width: 60%;
-        height: 550px;
+        width: 80%;
+        height: 200px;
         text-align: center;
     }
     .vd>h3 {
@@ -353,17 +417,19 @@
     }
     .vd>video {
         width: 100%;
-        height: 480px;
+        height: 180px;
     }
 
     /*课程介绍*/
     .Course_Introduction {
-        /*margin-top: 320px;*/
+        width: 100%;
+        /*background-color: red;*/
+        margin-top: 50px;
         text-align: center;
     }
 
     .Course_Introduction > p {
-        font-size: 20px;
+        font-size: 14px;
         color: #585B5E;
     }
 
@@ -376,7 +442,10 @@
         margin-top: 440px;
         text-align: center;
     }
-
+    .CI_content {
+        width: 100%;
+        /*margin-left:10%;*/
+    }
     .CE_content_left {
         text-align: left;
         font-size: 0;
@@ -388,13 +457,13 @@
     }
 
     /*备考学校*/
-    .CI_content_left {
-        margin-left: 20%;
-    }
+    /*.CI_content_left {*/
+    /*margin-left: 10%;*/
+    /*}*/
 
     .CI_content_left, .CI_content_middle, .CI_content_right {
         float: left;
-        width: 20%;
+        width: 33.33%;
         color: #3A3A3A;
     }
 
@@ -404,7 +473,14 @@
         height: 57px;
         line-height: 57px;
     }
-
+    .CI_content_left > p, .CI_content_middle > p, .CI_content_right > p {
+        font-size: 10px;
+        /*!*-webkit-transform-origin-x: 0;*!*/
+        /*-webkit-transform: scale(0.50);*/
+    }
+    .preparation_school{
+      margin-top:30px;
+    }
     .CI_content_middle > h3 {
         background: #797E82;
     }
@@ -425,8 +501,9 @@
     }
 
     .ps_footer {
+        width: 80%;
         margin-top: 30px;
-        margin-left: 30%;
+        margin-left: 10%;
     }
 
     .ps_footer h3 {
@@ -436,14 +513,16 @@
     .ps_footer-left, .ps_footer-right {
         float: left;
         text-align: left;
-        width: 30%;
+        width: 33%;
         float: left;
         color: #3A3A3A;
     }
-
+    .ps_footer-left {
+        margin-left:17%;
+    }
     .ps_footer-right > p {
         text-align: left;
-        margin-left: 100px;
+        margin-left: 120px;
     }
 
     .ps_footer:after {
