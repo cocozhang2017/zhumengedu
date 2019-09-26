43d6b1ebd380a95c31dae904ed0fefebf6512b3b
cocozhang <1275899401@qq.com>
2019-09-26 20:10:32 +0800

    联系我们移动端修改

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,11 +1,7 @@
 <template>
     <div>
         <div class="topBox">
-            <img src="../assets/mobile/contact/contact1.jpg"
-                 srcset="../assets/mobile/contact/contact1-assets/contact1@1x.png 320w,
-                        ../assets/mobile/contact/contact1-assets/contact1@2x.png 375w,
-                        ../assets/mobile/contact/contact1-assets/contact1@3x.png 414w"
-            >
+            <img src="../assets/img/contact0.png">
             <div class="top_middle">
                 <img src="../assets/img/contact4.png">
             </div>
@@ -13,27 +9,18 @@
         <!--联系我们很高兴为你服务-->
         <div class="happy">
             <div class="happy_img">
-                <img src="../assets/mobile/contact/contact2.jpg"
-                     srcset="../assets/mobile/contact/contact2-assets/contact2@1x.png 320w,
-                        ../assets/mobile/contact/contact2-assets/contact2@2x.png 375w,
-                        ../assets/mobile/contact/contact2-assets/contact2@3x.png 414w"
-                >
+                <img src="../assets/img/contact1.jpg" alt="">
             </div>
         </div>
         <!--联系我们 联系地址-->
         <div class="contact">
             <div class="contact-us">
                 <div class="contact-us-left">
-                    <img src="../assets/mobile/contact/contact3.jpg" alt=""
-                         srcset="../assets/mobile/contact/contact3-assets/contact3@1x.png 320w,
-                        ../assets/mobile/contact/contact3-assets/contact3@2x.png 375w,
-                        ../assets/mobile/contact/contact3-assets/contact3@3x.png 414w"
-                    >
+                    <img src="../assets/img/contact2.jpg" alt="">
                 </div>
                 <div class="contact-us-right">
                     <div class="contact-us-right-top">
-                        <img src="../assets/img/lxwm.png" alt=""
-                        >
+                        <img src="../assets/img/lxwm.png" alt="">
                         <span>联系我们</span>
                         <hr>
                     </div>
@@ -61,11 +48,7 @@
             </div>
             <div class="contact-address">
                 <div class="contact-address-left">
-                    <img src="../assets/mobile/contact/contact4.jpg" alt=""
-                         srcset="../assets/mobile/contact/contact4-assets/contact4@1x.png 320w,
-                        ../assets/mobile/contact/contact4-assets/contact4@2x.png 375w,
-                        ../assets/mobile/contact/contact4-assets/contact4@3x.png 414w"
-                    >
+                    <img src="../assets/img/contact3.jpg" alt="">
                 </div>
                 <div class="contact-address-right">
                     <div class="contact-address-right-top">
@@ -114,7 +97,7 @@
         text-align: center;
         color: white;
         position: absolute;
-        top:55%;
+        top: 55%;
         bottom: 0%;
         left: 50%;
         transform: translateX(-50%);
@@ -128,10 +111,12 @@
         position: relative;
         top: -160px;
     }
+
     .happy_img {
         width: 100%;
     }
-    .happy_img>img {
+
+    .happy_img > img {
         width: 100%;
         height: 100%;
     }
@@ -161,16 +146,19 @@
         flex: 1;
         margin: 50px 0 0 30px;
     }
+
     .contact-us-right-top {
         height: 60px;
     }
-    .contact-us-right-top>img,.contact-address-right-top>img {
+
+    .contact-us-right-top > img, .contact-address-right-top > img {
         height: 20px;
-        line-height:20px;
+        line-height: 20px;
     }
-    .contact-us-right-top>span,.contact-address-right-top>span {
-        display:inline-block;
-        line-height:20px;
+
+    .contact-us-right-top > span, .contact-address-right-top > span {
+        display: inline-block;
+        line-height: 20px;
         vertical-align: top;
     }
 </style>
